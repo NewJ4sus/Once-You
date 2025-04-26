67c6cdd13397f4776f563f84250f4153cd30950d
NewJ4sus <holiday0110@yandex.ru>
2025-04-26 13:43:34 +0300

    update local

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-// import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path';
 export default defineConfig({
     plugins: [
         react(),
         // VitePWA({
-        //     registerType: 'autoUpdate',
-        //     manifest: {
-        //         name: 'Once You',
-        //         short_name: 'Once You',
-        //         start_url: '/',
-        //         display: 'standalone',
-        //         background_color: '#141414',
-        //         theme_color: '#141414',
-        //         icons: [
-        //             {
-        //                 src: 'public/192.svg',
-        //                 sizes: '192x192',
-        //                 type: 'image/svg+xml'
-        //             },
-        //             {
-        //                 src: 'public/512.svg',
-        //                 sizes: '512x512',
-        //                 type: 'image/svg+xml'
-        //             }
-        //         ]
-        //     }
+        //   registerType: 'autoUpdate',
+        //   manifest: {
+        //     name: 'Once You',
+        //     short_name: 'Once You',
+        //     start_url: '/',
+        //     display: 'standalone',
+        //     background_color: '#141414',
+        //     theme_color: '#141414',
+        //     icons: [
+        //       {
+        //         src: 'public/192.svg',
+        //         sizes: '192x192',
+        //         type: 'image/svg+xml'
+        //       },
+        //       {
+        //         src: 'public/512.svg',
+        //         sizes: '512x512',
+        //         type: 'image/svg+xml'
+        //       }
+        //     ]
+        //   }
         // }),
     ],
     resolve: {
